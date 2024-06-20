dc8a0f74b98d010296673b0d4ffe48397d2016a8
Alec Gillis <gillismail1213@gmail.com>
2024-06-19 22:52:37 -0400

    Add comments

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"; // Import Vue framework
+import App from "./App.vue"; // Import the main App component
 
+// Create a new Vue instance
 new Vue({
-  render: h => h(App),
-}).$mount("#app");
+  render: h => h(App), // Render the App component into the root div
+}).$mount("#app"); // Mount the Vue instance to the div with id "app" in the HTML file
