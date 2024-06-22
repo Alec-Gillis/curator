03b82790c2908391c0b1cceedac1b504e4187400
Alec Gillis <gillismail1213@gmail.com>
2024-06-22 00:42:54 -0400

    Reject modernity, embrace tradition.

--- client/src/main.ts
+++
@@ -1,7 +0,0 @@
-import Vue from "vue"; // Import Vue framework
-import App from "./App.vue"; // Import the main App component
-
-// Create a new Vue instance
-new Vue({
-  render: h => h(App), // Render the App component into the root div
-}).$mount("#app"); // Mount the Vue instance to the div with id "app" in the HTML file
